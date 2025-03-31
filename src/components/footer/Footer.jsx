436e0bc3018097081748f6c92da4adbf6ca5fc44
Javohir708 <joxauzking@gamil.com>
2025-03-31 13:31:50 +0500

    fixed bugs footer + cursor pointer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,12 +15,12 @@ const Footer = () => {
                 </div>
 
                 <div>
-                <div className='flex items-center gap-[20px]'>
+                <div className='flex items-center gap-[20px] cursor-pointer'>
                     <img src={location} alt="location" />
                     <h2>O'zbekiston, Buxoro shaxri</h2>
                     <img src={arrow} alt="arrow image" />
                 </div>
-                <div className='flex items-center gap-[20px] mt-2'>
+                <div className='flex items-center gap-[20px] mt-2 cursor-pointer'>
                     <img src={phone} alt="location" />
                     <h2>+998 94 033 72 12</h2>
                     <img src={arrow} alt="arrow image" />   
